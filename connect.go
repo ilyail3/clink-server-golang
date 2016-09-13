54e6cc0bb366357cd8204bb245f71d6b08bbd1cc
Ilya Kramer <ikrammer@datapipe.com>
2016-09-13 15:48:16 -0400

    Add transaction rollback on fail

--- connect.go
+++ connect.go
@@ -6,6 +6,7 @@ import (
 	"encoding/json"
 	"github.com/satori/go.uuid"
 	"time"
+	"database/sql"
 )
 
 type ConnectionRequest struct {
@@ -27,7 +28,51 @@ func numberInSlice(a int, list []int) bool {
 	return false
 }
 
+func connectTransaction(tx *sql.Tx, session_id uuid.UUID, t time.Time, connect_request *ConnectionRequest) error{
+	_, err := tx.Exec("INSERT INTO sessions(session_id, server_id, user_id, connect_time, disconnect_time) " +
+		"VALUES (?, ?, ?, ?, 0)",
+		session_id.String(),
+		connect_request.TargetServerId,
+		connect_request.UserId,
+		t.Unix())
 
+	if err != nil {
+		return err
+	}
+
+	var source int
+	source = 0
+
+	for _, proxy_id := range connect_request.ProxyIds {
+		_, err = tx.Exec("INSERT INTO access_logs(log_id, server_id, time, source_server_id, session_id) " +
+			"VALUES (?, ?, ?, ?, ?)",
+			uuid.NewV4().String(),
+			proxy_id,
+			t.Unix(),
+			source,
+			session_id.String())
+
+		if err != nil {
+			return err
+		}
+
+		source = proxy_id
+	}
+
+	_, err = tx.Exec("INSERT INTO access_logs(log_id, server_id, time, source_server_id, session_id) " +
+		"VALUES (?, ?, ?, ?, ?)",
+		uuid.NewV4().String(),
+		connect_request.TargetServerId,
+		t.Unix(),
+		source,
+		session_id.String())
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
 
 func (s CLinkServer)Connect(w http.ResponseWriter, r *http.Request) {
 	body, err := ioutil.ReadAll(r.Body)
@@ -67,55 +112,20 @@ func (s CLinkServer)Connect(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	_, err = tx.Exec("INSERT INTO sessions(session_id, server_id, user_id, connect_time, disconnect_time) " +
-		"VALUES (?, ?, ?, ?, 0)",
-		session_id.String(),
-		connect_request.TargetServerId,
-		connect_request.UserId,
-		t.Unix())
+	err = connectTransaction(tx, session_id, t, &connect_request)
 
 	if err != nil {
-		s.Log.Error("Failed to write session:" + err.Error())
+		s.Log.Error("Failed to write connection:" + err.Error())
+
+		tx.Rollback()
+
 		s.InternalError(w)
-		return
+	} else {
+		tx.Commit()
 	}
 
-	var source int
-	source = 0
 
-	for _, proxy_id := range connect_request.ProxyIds {
-		_, err = tx.Exec("INSERT INTO access_logs(log_id, server_id, time, source_server_id, session_id) " +
-			"VALUES (?, ?, ?, ?, ?)",
-			uuid.NewV4().String(),
-			proxy_id,
-			t.Unix(),
-			source,
-			session_id.String())
 
-		if err != nil {
-			s.Log.Error("Failed to write access log:" + err.Error())
-			s.InternalError(w)
-			return
-		}
-
-		source = proxy_id
-	}
-
-	_, err = tx.Exec("INSERT INTO access_logs(log_id, server_id, time, source_server_id, session_id) " +
-		"VALUES (?, ?, ?, ?, ?)",
-		uuid.NewV4().String(),
-		connect_request.TargetServerId,
-		t.Unix(),
-		source,
-		session_id.String())
-
-	if err != nil {
-		s.Log.Error("Failed to write access log:" + err.Error())
-		s.InternalError(w)
-		return
-	}
-
-	tx.Commit()
 
 
 }
